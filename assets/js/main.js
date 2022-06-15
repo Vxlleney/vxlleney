7730126f95552f1e1d814d669acc7d4a6e7a9d15
Vxlleney <97960615+Vxlleney@users.noreply.github.com>
2022-06-16 00:15:25 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,58 @@
-// MENU
-function toggleMenu2() {
-    const navResponsive = document.querySelector('.nav-menu');
+// DARK AND LIGHT THEME
+const themeButton = document.getElementById('theme_change')
+const themeSelector = localStorage.getItem('theme');
 
-    if(navResponsive.classList.contains('active')) {
-        navResponsive.classList.remove('active');
-    } else {
-        navResponsive.classList.add('active');
+    if (themeSelector == 'dark'){
+        themeButton.href = "assets/css/dark_styles.css";  
     }
 
-}
-
-function toggleMenu() {
-    const navResponsive = document.querySelector('.nav-menu');
-
-    if(navResponsive.classList.contains('active')) {
-        navResponsive.classList.remove('active');
-    } else {
-        navResponsive.classList.add('active');
+    else {
+        themeButton.href = "assets/css/styles.css";  
     }
-}
 
 function toggleTheme() {
-    var styles = document.getElementById("theme_change");
-    var nav = document.getElementById("nav_theme_change");
-    
-    if (styles.href.match("assets/css/dark_styles.css")) {
-        styles.href = "assets/css/styles.css";    
-    }
-    else {
-        styles.href = "assets/css/dark_styles.css";  
+
+    localStorage.removeItem('theme');
+
+    if (themeButton.href.match("assets/css/dark_styles.css")) {
+        themeButton.href = "assets/css/styles.css";  
+
+        localStorage.setItem('theme', 'light');  
     }
 
-    if (nav.href.match("assets/css/dark_nav-styles.css")) {
-        nav.href = "assets/css/nav-styles.css";    
-    }
     else {
-        nav.href = "assets/css/dark_nav-styles.css";  
+        themeButton.href = "assets/css/dark_styles.css";  
+
+        localStorage.setItem('theme', 'dark');  
     }
 }
+
+
+
+
+// MENU RESPONSIVE
+const navResponsive = document.querySelector('.nav-menu');
+
+function toggleMenu() {
+
+    if(navResponsive.classList.contains('active')) {
+        navResponsive.classList.remove('active');
+    } else {
+        navResponsive.classList.add('active');
+    }
+}
+
+
+
+
+// SCROLL UP
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+// THEME CHANGER
 
