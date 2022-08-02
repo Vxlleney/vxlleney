8fcb89e746f65710aee9bfb41675645ab83a03d9
Vxlleney <97960615+Vxlleney@users.noreply.github.com>
2022-08-02 14:57:22 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,5 +54,13 @@ function scrollUp(){
 window.addEventListener('scroll', scrollUp)
 
 
-// THEME CHANGER
+// LOADEr
+
+const fadeOut = () => {
+    const loaderWrapper =
+    document.querySelector('.wrapper');
+    loaderWrapper.classList.add('fade');
+}
+
+window.addEventListener('load', fadeOut);
 
